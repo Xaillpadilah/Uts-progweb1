7bd4f025556a8c0026aaeaa4135f920c56f82a9a
Xaillpadilah <149940955+Xaillpadilah@users.noreply.github.com>
2023-11-27 12:01:33 +0700

    Add files via upload

---
+++ data_pendaptar.php
@@ -0,0 +1,16 @@
+<?php
+require '../koneksi/koneksi.php';
+
+try{
+    $Sql = 'SELECT * FROM data_pendaftar';
+    $qonnect = $database_connection->prepare($Sql);
+    $qonnect->execute();
+
+    $data = array();
+    while ($row = $qonnect->fetch(PDO::FETCH_ASSOC)) {
+        array_push($data,$row);
+    }
+    echo json_encode($data);   
+}catch (PDOException $err) {
+die("tidak dapat memuat database $database_name:". $err->getMessage());
+}
